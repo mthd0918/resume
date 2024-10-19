2ef5821c0c59a1afdb993c19edbb32abc62d2615
Hidechika Matayoshi <hmtys0918@icloud.com>
2024-10-19 16:58:25 +0900

    fix: layout, bug, jsonfile

--- src/js/script.js
+++ src/js/script.js
@@ -1,35 +1,23 @@
-/* <div class="project-cards">
-    <div class="container d-flex justify-content-center flex-wrap text-center">
-        <div class="bg-light col-12 col-md-6 col-lg-3 p-4">
-            <h3>title</h3>
-            <p>summary</p>
-            <button class="btn btn-primary">show more</button>
-        </div>
-    </div>
-</div> */
-
 function createProjectCards() {
-    fetch('../project.json')
-        .then(response => {
-            return response.json();
-        })
+    fetch('./project.json')
+        .then(response => response.json())
         .then(data => {
             const projectCards = document.querySelector('.project-cards');
             const row = document.createElement('div');
             row.className = 'row pb-2';
-
-            data.forEach(project => {
+            // データを逆順に表示
+            data.reverse().forEach(project => {
                 const col = document.createElement('div');
                 col.className = 'col-12 col-md-6 col-lg-4 p-2';
                 
                 const card = document.createElement('div');
-                card.className = 'card h-100'; // h-100 ensures all cards in a row have the same height
+                card.className = 'card h-100';
                 
                 card.innerHTML = `
                     <div class="card-body d-flex flex-column">
                         <h3 class="card-title">${project.title}</h3>
                         <p class="card-text flex-grow-1">${project.summary}</p>
-                        <a href="${project.link}" class="btn btn-primary mt-auto" target="_blank">show more</a>
+                        <a href="${project.link}" class="btn btn-outline-primary mt-auto" target="_blank">show more</a>
                     </div>
                 `;
                 
